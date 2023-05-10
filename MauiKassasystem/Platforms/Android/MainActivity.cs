ec032d57a8463a98ea84667190cc4ed353bd0af6
Pascal Serghei <syryoga1@gmail.com>
2023-05-10 17:09:49 +0200

    CSS änderungen

--- MauiKassasystem/Platforms/Android/MainActivity.cs
+++ MauiKassasystem/Platforms/Android/MainActivity.cs
@@ -4,7 +4,15 @@ using Android.OS;
 
 namespace MauiKassasystem;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", 
+    MainLauncher = true, 
+    ConfigurationChanges = 
+    ConfigChanges.ScreenSize | 
+    ConfigChanges.Orientation | 
+    ConfigChanges.UiMode | 
+    ConfigChanges.ScreenLayout | 
+    ConfigChanges.SmallestScreenSize | 
+    ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
