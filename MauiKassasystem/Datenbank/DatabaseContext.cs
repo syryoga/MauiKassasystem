c5fdfe4bcd2b5f616f53ad0a29bb3c075bb11520
Gerald <gerald.hradil@gmx.at>
2023-05-17 13:25:24 +0200

    Warnfenster hinzugefügt beim Storno Button auf der MainPage

--- MauiKassasystem/Datenbank/DatabaseContext.cs
+++ MauiKassasystem/Datenbank/DatabaseContext.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Devices.AllJoyn;
+//using Windows.Devices.AllJoyn;
 
 namespace MauiKassasystem.Datenbank
 {
