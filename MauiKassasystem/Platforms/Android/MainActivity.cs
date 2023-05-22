4a9a75620afe44ed542c7e560808efcf3d2f2387
Pascal Serghei <syryoga1@gmail.com>
2023-05-22 11:17:23 +0200

    App-Logo geändert und Lpgo in boot geändert

--- MauiKassasystem/Platforms/Android/MainActivity.cs
+++ MauiKassasystem/Platforms/Android/MainActivity.cs
@@ -22,34 +22,5 @@ namespace MauiKassasystem;
     ScreenOrientation = ScreenOrientation.Landscape)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Bundle savedInstanceState)
-    {
-        base.OnCreate(savedInstanceState);
-        global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
-
-        global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-        LoadApplication(new App());
-
-        SetBottomNavigationViewColors();
-    }
-
-    private void SetBottomNavigationViewColors()
-    {
-        // Получаем ресурсные идентификаторы цветов из XML-файла ресурсов
-        var resources = Resources;
-        var backgroundColorId = resources.GetIdentifier("bottom_nav_background", "color", PackageName);
-        var selectedColorId = resources.GetIdentifier("bottom_nav_selected", "color", PackageName);
-        var backgroundColor = resources.GetColor(backgroundColorId);
-        var selectedColor = resources.GetColor(selectedColorId);
-
-        // Находим BottomNavigationView по идентификатору
-        var bottomNavigationView = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation_view);
-
-        // Устанавливаем цвет фона и выделения
-        bottomNavigationView.SetBackgroundColor(backgroundColor);
-        bottomNavigationView.ItemRippleColor = new ColorStateList(
-            new int[][] { new int[] { } },
-            new int[] { selectedColor }
-        );
-    }
+    
 }
