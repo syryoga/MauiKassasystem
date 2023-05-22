e16bc70952c31fc7dd89dea5ea9d25cbc58e3c32
Pascal Serghei <syryoga1@gmail.com>
2023-05-22 10:30:19 +0200

    App-Logo geändert und Logo im boot geändert

--- MauiKassasystem/Platforms/Android/MainActivity.cs
+++ MauiKassasystem/Platforms/Android/MainActivity.cs
@@ -1,6 +1,12 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Content.Res;
 using Android.OS;
+using Google.Android.Material.BottomNavigation;
+//using Android.Support.Design.Widget;
+using Android.Widget;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
 
 namespace MauiKassasystem;
 
@@ -16,4 +22,34 @@ namespace MauiKassasystem;
     ScreenOrientation = ScreenOrientation.Landscape)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
+
+        global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+        LoadApplication(new App());
+
+        SetBottomNavigationViewColors();
+    }
+
+    private void SetBottomNavigationViewColors()
+    {
+        // Получаем ресурсные идентификаторы цветов из XML-файла ресурсов
+        var resources = Resources;
+        var backgroundColorId = resources.GetIdentifier("bottom_nav_background", "color", PackageName);
+        var selectedColorId = resources.GetIdentifier("bottom_nav_selected", "color", PackageName);
+        var backgroundColor = resources.GetColor(backgroundColorId);
+        var selectedColor = resources.GetColor(selectedColorId);
+
+        // Находим BottomNavigationView по идентификатору
+        var bottomNavigationView = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation_view);
+
+        // Устанавливаем цвет фона и выделения
+        bottomNavigationView.SetBackgroundColor(backgroundColor);
+        bottomNavigationView.ItemRippleColor = new ColorStateList(
+            new int[][] { new int[] { } },
+            new int[] { selectedColor }
+        );
+    }
 }
